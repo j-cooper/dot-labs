632745b5cc1ad90a9fb4d2c638dbbc188a22e390
Yury Yatsevich <yury.yatsevich1@external.danone.com>
2024-12-19 16:04:55 +0100

    edit dim model

--- models/staging/tpch_sf100/stg_tpch_sf100_cust.sql
+++ models/staging/tpch_sf100/stg_tpch_sf100_cust.sql
@@ -8,7 +8,7 @@ with cust as (
  select c_custkey,
         c_name,
         c_address
- from snowflake_sample_data.tpch_sf100.customer
+ from {{ source('tpch_sf100', 'customer') }}
  where substr(c_phone, 1, 2) = '15')
 
 select cust.*
--- models/staging/tpch_sf100/stg_tpch_sf100_order.sql
+++ models/staging/tpch_sf100/stg_tpch_sf100_order.sql
@@ -6,7 +6,7 @@
 
 with ord as (
  select *
- from snowflake_sample_data.tpch_sf100.orders
+ from {{ source('tpch_sf100', 'orders') }}
  where substr(o_orderdate, 1, 4) = 1995)
 
 select ord.*
