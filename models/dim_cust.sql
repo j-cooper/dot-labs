b5ed1bc33d4b234f979a002e8ac8952d012b9096
Yury Yatsevich <yury.yatsevich1@external.danone.com>
2024-12-19 14:19:44 +0100

    models update

---
+++ models/dim_cust.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with cust as (
+ select c_custkey,
+        c_name,
+        c_address
+ from snowflake_sample_data.tpch_sf100.customer
+ where substr(c_phone, 1, 2) = '15')
+
+select cust.*
+from cust
